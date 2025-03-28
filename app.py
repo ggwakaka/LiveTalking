e140cd270553920bb0e6e183704f03ce872d812b
gaogang <809537398@qq.com>
2025-03-28 17:20:14 +0800

    增加start方法

--- app.py
+++ app.py
@@ -281,6 +281,7 @@ async def is_speaking(request):
 async def start(request):
     form = await request.post()
     sessionid = int(form.get('sessionid', 0))
+    logger.info(json.dumps(form))
     logger.info(f'sessionid={sessionid}')
     if sessionid == 0:
         return web.Response(
